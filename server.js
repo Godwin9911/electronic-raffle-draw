97e87600b4699b7090fc61cba32c0984f8cbe0f6
Godwin9911 <agedah99@gmail.com>
2019-08-13 00:13:54 +0100

    redo

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.get('/contestants', (req,res)=>{
 })
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static( 'client/build'));
   app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  })
+  });
 }
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
